f6b2a44f31c56d010fec24d6f81386d528cbd657
Joan Gamell <gamell@gmail.com>
2017-06-18 23:57:29 -0700

    braveclojure example

---
+++ braveclojure/clojure-noob/profiles.clj
@@ -0,0 +1,3 @@
+{:debug-repl {:resource-paths [#=(eval (System/getenv "PATH_TO_TOOLS_JAR"))]
+              :repl-options {:nrepl-middleware [debug-middleware.core/debug-middleware]}
+              :dependencies [[debug-middleware #=(eval (System/getenv "DEBUG_MIDDLEWARE_VERSION"))]]}}
